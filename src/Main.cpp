a0e15b7b2d4421c38bad7b4ec97463dd07cc893b
josip <josipkeresman@gmail.com>
2024-08-24 22:06:36 +0200

    Remove stuff that isn't used

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,12 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-#include "headers/ResourceManager.h"
 #include "headers/Spaceship.h"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML Window");
 
-    space::ResourceManager resourceManager;
     space::Spaceship spaceship;
 
     while (window.isOpen())

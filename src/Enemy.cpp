157a387dfbe4132906d2dcf7cc2feab06ff7d745
josip <josipkeresman@gmail.com>
2024-08-27 18:41:14 +0200

    Small refactor in Enemy impl

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -16,7 +16,7 @@ Enemy::Enemy(float positionX, float positionY)
 
 void Enemy::changeDirection()
 {
-    m_direction = m_direction == Direction::RIGHT ? Direction::LEFT : Direction::RIGHT;
+    m_direction = (m_direction == Direction::RIGHT) ? Direction::LEFT : Direction::RIGHT;
 
     sf::Vector2f position = m_enemy.getPosition();
     position.y += 40.0f;
